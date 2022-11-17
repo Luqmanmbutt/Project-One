9d518ff8409d68e27d5103056ac965011276e489
Luqman Butt <luqmanmbutt1@gmail.com>
2022-11-17 17:47:40 +0000

    snake now moves left and right and the grid is now bigger with smaller divs inside

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,23 @@
 function init() {
-  //Hamza's suggestion: Take code line 4, 6, 7, 8, 17-25 (not take 'addCat' on line 24)
+  
   // * Variables
   const grid = document.querySelector(".grid") // get the grid element
   
-  const width = 10 // define the width ---- YOU STORE THINGS INSIDE A VARIABLE.
+  const width = 20 // define the width ---- YOU STORE THINGS INSIDE A VARIABLE.
   const cellCount = width * width // define the number of cells on the grid
   const cells = [] // empty array to store our divs that we create
 
   const foodForSnake = "food" 
-  const foodStartPosition = Math.floor(Math.random() * 100)
+  const foodStartPosition = Math.floor(Math.random() * 400)
   // let foodCurrentPostion = 0
 
   const snakeClass = "snake" // define the class of the character
   const snakeStartPosition = [44, 43, 42]// starting position of the cat (refers to an index)
-  let snakeCurrentPosition = 44  // use let to track where the cat currently is (refers to an index)
-// 
+  let snakeCurrentPosition = [44, 43, 42]  // use let to track where the cat currently is (refers to an 
+  let snakeDirection 
+  let snakeTimer 
+  const speed = 500
+
 
   // * Make a grid
   const createGrid = (snakeStartPosition) => {
@@ -38,22 +41,23 @@ const addFood = (position) => {
 // foodCurrentPostion = Math.floor(Math.random() * 100)
 
 
-  const addSnake = (position) => { 
+  const addSnake = () => { 
   /* snakeStartPosition is an array with 3 numbers [42, 43, 44] 
      the forEach breaks the array, and accesses 3 the numbers and stores them in 'x'. so x = 44, x = 43 and x = 42
      then whatever you write inside the foreach BLOCK, it does that command to each of those numbers. 
   */ 
-     snakeStartPosition.forEach(x => {
+     snakeCurrentPosition.forEach(x => {
       cells[x].classList.add(snakeClass) 
-      console.log('test', snakeStartPosition)
     })
   
   }
 
 // * Remove snake from current position 
-const removeSnake = (position) => {
-    cells[position].classList.remove(snakeClass)
-    // snakeStartPosition.pop()
+const removeSnake = () => {
+  snakeCurrentPosition.forEach(x => {
+    cells[x].classList.remove(snakeClass) 
+  })
+
 }
 
 
@@ -62,22 +66,54 @@ const removeSnake = (position) => {
 
 const snakeMove = () => {
   
-  // console.log('test')
+  clearInterval(snakeTimer)
+  snakeTimer = setInterval(() => {
+    
+    
+    
+    if (snakeDirection === 'right' && snakeCurrentPosition[0] % width !== width - 1) {
+      removeSnake()
+      console.log('snakeCurrentPosition before', snakeCurrentPosition)
+      snakeCurrentPosition.unshift(snakeCurrentPosition[0] + 1) // add a new item (number) to the beginning of the array 
+      snakeCurrentPosition.pop()
+      console.log('snakeCurrentPosition after', snakeCurrentPosition)
+      addSnake()
+
+    } else if (snakeDirection === 'left' && snakeCurrentPosition[0] % width !== 0) {
+      removeSnake()
+      // console.log('snakeCurrentPosition before', snakeCurrentPosition)
+      snakeCurrentPosition.push(snakeCurrentPosition[2] - 1)
+      snakeCurrentPosition.shift()
+      // console.log('snakeCurrentPosition after', snakeCurrentPosition)
+      addSnake() 
+    } else if (snakeDirection === 'up' && snakeCurrentPosition[0] >= width) {
+      console.log('snakeCurrentPosition before', snakeCurrentPosition)
+
+
+    }
   
+  
+  
+  
+  
+  }, speed) 
+
+   
 }
 
-setInterval(snakeMove, 1000)
+// 44 % 10 = 4 without the remainder 
+// The unshift() method adds new elements to the beginning of an array
+// The pop() method removes (pops) the last element of an array.
+// 44 % 10 = 4 without the remainder 
+// with the function snakeMove we are trying to make the snake move constantly right how we do that. 
+// the function snakeMove clear snake use the id returned from setInterval which is snakeTimer. 
+// setINterval function saying if snakedirection equlas to right and snakecurrentposition is ([0](44)) 
+// - 1 to remove last snake 
+// snakecurrentposition.unshift means adding a new element in the begingin of the array which than adds +1
+// to the array [0] while removing the last number from the array an adding the snake to the next move 
+//
+//
 
-snakeMove()
-
-// const moveSnake = document.getElementById("demo");
-// setInterval(function() {element.innerHTML += "Hello"}, 1000);
-
-// console.log(moveSnake)
-
-// const setInterval = () => {
-//    {element.innerHTML += 'hello'}, 1000;
-// }
 
 
 const handleKeyUp = (event) => {
@@ -85,16 +121,17 @@ const handleKeyUp = (event) => {
   //  
   removeSnake(snakeCurrentPosition) // remove the cat from its current position
   
-  // when firing handlekeyup both remove and add snake are being fired within the same function.
-// removes the snake only after its fired again - line by line code- 
-  if (key === 39 && snakeCurrentPosition % width !== width - 1) { // if the right arrow is pressed and the cat is not on the right edge
-    snakeCurrentPosition++ // add 1 to the current position.
-  } else if (key === 37 && snakeCurrentPosition % width !== 0) { // if the left arrow is pressed and the cat is not on the left edge
-    snakeCurrentPosition-- // removes 1 from the current position
-  } else if (key === 38 && snakeCurrentPosition >= width) { // if the up arrow is pressed and the cat is not on the top row
-    snakeCurrentPosition -= width // minus 10 from current position
-  } else if (key === 40 && snakeCurrentPosition + width <= width * width - 1) { // if the down arrow is pressed and the cat is not on the bottom row
-    snakeCurrentPosition += width // adds 10 to current position
+ 
+  if (key === 39) {
+    snakeDirection = 'right' 
+    snakeMove()
+  } else if (key === 37) { 
+    snakeDirection = 'left'
+    snakeMove() 
+  } else if (key === 38 && snakeCurrentPosition >= width) { 
+    snakeCurrentPosition -= width 
+  } else if (key === 40 && snakeCurrentPosition + width <= width * width - 1) { 
+    snakeCurrentPosition += width 
   } else {
     // console.log(‘INVALID KEY’) // any other key, log invalid key
   }
@@ -105,6 +142,7 @@ const handleKeyUp = (event) => {
 }
 
 
+
 // * Event listeners
 document.addEventListener("keyup", handleKeyUp) // 
 
@@ -113,15 +151,37 @@ createGrid(snakeStartPosition) // pass function the starting position of the cat
 // handleKeyUp()
 
 
+// MOVE SNAKE CONSTANTLY ----------- work on this after creating a new variable -- mae sure to change the variable name on the function below.
 
+// const handleKeyUp = (event) => {
+//   const key = event.keyCode // press button 
+//   //  
+//   removeSnake(snakeCurrentPosition) // remove the cat from its current position
+  
+//   // when firing handlekeyup both remove and add snake are being fired within the same function.
+// // removes the snake only after its fired again - line by line code- 
+//   if (key === 39 && snakeCurrentPosition % width !== width - 1) { // if the right arrow is pressed and the cat is not on the right edge
+//     snakeCurrentPosition++ // add 1 to the current position.
+//   } else if (key === 37) { // if the left arrow is pressed and the cat is not on the left edge
+//     snakeCurrentPosition-- // removes 1 from the current position
+//   } else if (key === 38 && snakeCurrentPosition >= width) { // if the up arrow is pressed and the cat is not on the top row
+//     snakeCurrentPosition -= width // minus 10 from current position
+//   } else if (key === 40 && snakeCurrentPosition + width <= width * width - 1) { // if the down arrow is pressed and the cat is not on the bottom row
+//     snakeCurrentPosition += width // adds 10 to current position
+//   } else {
+//     // console.log(‘INVALID KEY’) // any other key, log invalid key
+//   }
+// //   console.log(‘POSITION AFTER REDEFINING —>’, catCurrentPosition)
+//   addSnake(snakeCurrentPosition) // add cat to the new position that was defined in the if statement above
+  
 
+// }
 
 
 
 
 
 }
-
 
 
 window.addEventListener("DOMContentLoaded", init)
--- scripts/planning.js
+++ scripts/planning.js
@@ -4,7 +4,7 @@
 //  the same way i added the snake will be the same way i add food DONE 
 //    now food need to fire once snake touches the food.
 //     when snake touched food, food need to be removed and apear in a diffrent div
-
+//
 // Snake 
 // increase Snake speed every time it touches food
 // increase the size of the snake after it touches the food 
@@ -14,24 +14,93 @@
 // snake dies if it touches himself 
 //  only clearinterval when snake dies
 //
-//  new variable called snakeDirection which will be updated with strings up, down, left, right 
+//  new variable called snakeDirection which will be 
+//  updated with strings up, down, left, right 
+//
+// 
+// .How you call an array 
+//  to call a an item from the array you have to put x => as a function 
+//  to return x which in this case is an array with the item 44,43.42 inside, 
+//   (in this case we are adding 1 each time to x )
 //
 //
+//   snake moves. when you press key snake should move from starting position, 
+//   keep adding the number from each direction ( right meaning add 1 continously) switch statement ??
+//   
+
+
+
+
+
+
+
+
+
+
 //
-//
-//
-
-
-
-
-
-
 // 1. Snake needs to keep moving  up down left right DONE
 // 2. Snake to die if it hit the edge 
 // 3. Randomise the food
-
+//
 // 4. How to start the game 
 // 5. How to finish the game 
 // 6. How to restart the game 
 // 7. Snake needs to grow every time it eats
 // 8. 
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
